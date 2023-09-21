c52d36c7150a35bf72b9e010688330900240492b
Shiv Singh <140262444+shivsingh98@users.noreply.github.com>
2023-09-21 01:13:35 +0000

    initial commit

---
+++ Day 3/First.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    std::cout<<"Hello World!!"<< endl;
+    std::cout<<"Hello World!!"<< endl;
+    std::cout<<"Hello World!!"<< endl;
+    std::cout<<"Hello World!!"<< endl << "I Love You\n" ;
+    std::cout<<"2 + 3"<< endl;
+    std::cout<<2 + 3<< endl;
+
+};
